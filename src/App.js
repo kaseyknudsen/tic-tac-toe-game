3e407767f5618eb96cb7e76735df6ff02b276bac
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-03-12 23:37:31 -0700

    got first box to work

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
 import { ButtonGroup, Button, Typography } from "@mui/material";
+import { useState } from "react";
 
 function App() {
+  let [counter, setCounter] = useState(0);
+  const [grid, setGrid] = useState({
+    box1: "",
+    box2: "",
+    box3: "",
+    box4: "",
+    box5: "",
+    box6: "",
+    box7: "",
+    box8: "",
+    box9: "",
+  });
+
+  const updateBox = () => {
+    let letter;
+    setCounter(counter + 1);
+    counter % 2 === 0 ? (letter = "X") : (letter = "O");
+    setGrid({box1: letter})
+  };
+
   return (
     <div className="App">
       <Typography variant="h2" mb={4}>
@@ -9,7 +30,18 @@ function App() {
       <ButtonGroup
         disableElevation
         variant="contained"
-        // aria-label="outlined primary button group"
+        color="info"
+        sx={{ columnGap: 1, marginBottom: 1 }}
+      >
+        <Button sx={{ p: 3 }} onClick={() => updateBox()}>
+          {grid.box1}
+        </Button>
+        <Button sx={{ p: 3 }}></Button>
+        <Button sx={{ p: 3 }}></Button>
+      </ButtonGroup>
+      <ButtonGroup
+        disableElevation
+        variant="contained"
         color="info"
         sx={{ columnGap: 1, marginBottom: 1 }}
       >
@@ -20,18 +52,6 @@ function App() {
       <ButtonGroup
         disableElevation
         variant="contained"
-        aria-label="outlined primary button group"
-        color="info"
-        sx={{ columnGap: 1, marginBottom: 1 }}
-      >
-        <Button sx={{ p: 3 }}></Button>
-        <Button sx={{ p: 3 }}></Button>
-        <Button sx={{ p: 3 }}></Button>
-      </ButtonGroup>
-      <ButtonGroup
-        disableElevation
-        variant="contained"
-        aria-label="outlined primary button group"
         color="info"
         sx={{ columnGap: 1 }}
       >
