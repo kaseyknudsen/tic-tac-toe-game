eee35e63e9ea115e677e8936e245f780e63b931e
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-03-12 22:39:02 -0700

    initial setup

--- src/App.js
+++ src/App.js
@@ -3,35 +3,37 @@ import { ButtonGroup, Button, Typography } from "@mui/material";
 function App() {
   return (
     <div className="App">
-      <Typography variant="h2" mb={4} >Tic Tac Toe</Typography>
+      <Typography variant="h2" mb={4}>
+        Tic Tac Toe
+      </Typography>
       <ButtonGroup
+        disableElevation
         variant="contained"
-        aria-label="outlined primary button group"
+        // aria-label="outlined primary button group"
         color="info"
-        size="medium"
-        sx={{ columnGap: 2, marginBottom: 1 }}
+        sx={{ columnGap: 1, marginBottom: 1 }}
       >
         <Button sx={{ p: 3 }}></Button>
         <Button sx={{ p: 3 }}></Button>
         <Button sx={{ p: 3 }}></Button>
       </ButtonGroup>
       <ButtonGroup
+        disableElevation
         variant="contained"
         aria-label="outlined primary button group"
         color="info"
-        size="medium"
-        sx={{ columnGap: 2, marginBottom: 1 }}
+        sx={{ columnGap: 1, marginBottom: 1 }}
       >
         <Button sx={{ p: 3 }}></Button>
         <Button sx={{ p: 3 }}></Button>
         <Button sx={{ p: 3 }}></Button>
       </ButtonGroup>
       <ButtonGroup
+        disableElevation
         variant="contained"
         aria-label="outlined primary button group"
         color="info"
-        size="medium"
-        sx={{ columnGap: 2 }}
+        sx={{ columnGap: 1 }}
       >
         <Button sx={{ p: 3 }}></Button>
         <Button sx={{ p: 3 }}></Button>
